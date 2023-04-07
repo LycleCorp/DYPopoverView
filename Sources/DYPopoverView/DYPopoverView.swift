80984a5e2e0d77076b202279e4e6d93c38af43ad
MussaCharles <mussacharles50@gmail.com>
2023-04-07 16:33:28 +0900

    Add alertenative method to show popover without without view being offset outside device screen bounds

--- Sources/DYPopoverView/DYPopoverView.swift
+++ Sources/DYPopoverView/DYPopoverView.swift
@@ -76,6 +76,159 @@ struct PopoverViewModifier<ContentView: View, BackgroundView: View>: ViewModifie
              .animation(settings.animation)
           
       }
+    
+    internal func popoverViewVertical<ContentView: View, BackgroundView: View>(
+        geometry: GeometryProxy?,
+        preferences: [DYPopoverViewOriginPreference],
+        popoverType: PopoverType,
+        @ViewBuilder content:  @escaping ()->ContentView,
+        isPresented: Binding<Bool>,
+        frame: Binding<CGRect>,
+        anchorFrame: CGRect?,
+        background: @escaping ()->BackgroundView,
+        position: ViewPosition,
+        viewId: String,
+        settings: DYPopoverViewSettings = DYPopoverViewSettings()
+    ) -> some View {
+        
+        var originBounds = CGRect.zero
+        if let anchorFrame = anchorFrame {
+            originBounds = anchorFrame
+        } else if let originPreference = preferences.first(where: { $0.viewId == viewId }), let geometry = geometry {
+           originBounds = geometry[originPreference.bounds]
+        }
+     
+
+         return
+            content()
+             .modifier(PopoverFrame(isPresented: isPresented, viewFrame: frame.wrappedValue, originBounds: originBounds, popoverType: popoverType))
+            .background(background().frame(width:frame.wrappedValue.width + settings.arrowLength * 2, height: frame.wrappedValue.height + settings.arrowLength * 2))
+             .opacity(isPresented.wrappedValue ? 1 : 0)
+            .clipShape(RoundedArrowRectangle(arrowPosition: self.arrowPosition(viewPosition: position, settings: settings), arrowLength: settings.arrowLength, cornerRadius: settings.cornerRadius))
+            .shadow(color: settings.shadowColor, radius: settings.shadowRadius)
+             .modifier(
+                 PopoverOffsetVertical(isPresented: isPresented,
+                 popoverType: popoverType,
+                 position: position,
+                xOffset: settings.offset.width,
+                yOffset: settings.offset.height,
+                arrowLength: settings.arrowLength)
+             )
+             .animation(settings.animation)
+          
+      }
+     
+    internal func arrowPosition(viewPosition: ViewPosition, settings: DYPopoverViewSettings)->ViewPosition {
+         
+         return settings.differentArrowPosition == .none ? viewPosition.opposite : settings.differentArrowPosition
+
+     }
+}
+
+struct PopoverViewModifierVertical<ContentView: View, BackgroundView: View>: ViewModifier {
+    
+    var contentView: ()->ContentView
+    var backgroundView: ()->BackgroundView
+    @Binding var show: Bool
+    @Binding var frame: CGRect
+    var anchorFrame: CGRect?
+    var popoverType: PopoverType
+    var position: ViewPosition
+    var viewId: String
+    var settings: DYPopoverViewSettings
+    
+     func body(content: Content) -> some View {
+        Group {
+            if anchorFrame != nil {
+
+              ZStack(alignment: .topLeading) {
+                
+                content
+                
+                self.popoverViewVertical(geometry: nil, preferences: [], popoverType: self.popoverType, content: self.contentView, isPresented: self.$show, frame: self.$frame, anchorFrame: self.anchorFrame, background: self.backgroundView, position: self.position, viewId: viewId, settings: self.settings)
+
+              }.edgesIgnoringSafeArea(.all)
+         
+            } else {
+               content
+                    .overlayPreferenceValue(DYPopoverViewOriginPreferenceKey.self) { preferences in
+
+                         return GeometryReader { geometry in
+                            ZStack(alignment: .topLeading) {
+                                 
+                                return self.popoverViewVertical(geometry: geometry, preferences: preferences, popoverType: self.popoverType, content: self.contentView, isPresented: self.$show, frame: self.$frame, anchorFrame: nil, background: self.backgroundView, position: self.position, viewId: self.viewId, settings: self.settings)
+
+                            }
+                           
+                         }
+
+                 }
+            }
+        }
+    }
+    
+    internal func popoverView<ContentView: View, BackgroundView: View>(geometry: GeometryProxy?, preferences: [DYPopoverViewOriginPreference], popoverType: PopoverType, @ViewBuilder content:  @escaping ()->ContentView, isPresented: Binding<Bool>, frame: Binding<CGRect>, anchorFrame: CGRect?, background: @escaping ()->BackgroundView,  position: ViewPosition, viewId: String, settings: DYPopoverViewSettings = DYPopoverViewSettings()) -> some View {
+        
+        var originBounds = CGRect.zero
+        if let anchorFrame = anchorFrame {
+            originBounds = anchorFrame
+        } else if let originPreference = preferences.first(where: { $0.viewId == viewId }), let geometry = geometry {
+           originBounds = geometry[originPreference.bounds]
+        }
+     
+
+         return
+            content()
+             .modifier(PopoverFrame(isPresented: isPresented, viewFrame: frame.wrappedValue, originBounds: originBounds, popoverType: popoverType))
+            .background(background().frame(width:frame.wrappedValue.width + settings.arrowLength * 2, height: frame.wrappedValue.height + settings.arrowLength * 2))
+             .opacity(isPresented.wrappedValue ? 1 : 0)
+            .clipShape(RoundedArrowRectangle(arrowPosition: self.arrowPosition(viewPosition: position, settings: settings), arrowLength: settings.arrowLength, cornerRadius: settings.cornerRadius))
+            .shadow(color: settings.shadowColor, radius: settings.shadowRadius)
+             .modifier(PopoverOffset(isPresented: isPresented, viewFrame: frame.wrappedValue, originBounds: originBounds, popoverType: popoverType, position: position, addOffset: settings.offset, arrowLength: settings.arrowLength))
+             .animation(settings.animation)
+          
+      }
+    
+    internal func popoverViewVertical<ContentView: View, BackgroundView: View>(
+        geometry: GeometryProxy?,
+        preferences: [DYPopoverViewOriginPreference],
+        popoverType: PopoverType,
+        @ViewBuilder content:  @escaping ()->ContentView,
+        isPresented: Binding<Bool>,
+        frame: Binding<CGRect>,
+        anchorFrame: CGRect?,
+        background: @escaping ()->BackgroundView,
+        position: ViewPosition,
+        viewId: String,
+        settings: DYPopoverViewSettings = DYPopoverViewSettings()
+    ) -> some View {
+        
+        var originBounds = CGRect.zero
+        if let anchorFrame = anchorFrame {
+            originBounds = anchorFrame
+        } else if let originPreference = preferences.first(where: { $0.viewId == viewId }), let geometry = geometry {
+           originBounds = geometry[originPreference.bounds]
+        }
+     
+
+         return
+            content()
+             .modifier(PopoverFrame(isPresented: isPresented, viewFrame: frame.wrappedValue, originBounds: originBounds, popoverType: popoverType))
+            .background(background().frame(width:frame.wrappedValue.width + settings.arrowLength * 2, height: frame.wrappedValue.height + settings.arrowLength * 2))
+             .opacity(isPresented.wrappedValue ? 1 : 0)
+            .clipShape(RoundedArrowRectangle(arrowPosition: self.arrowPosition(viewPosition: position, settings: settings), arrowLength: settings.arrowLength, cornerRadius: settings.cornerRadius))
+            .shadow(color: settings.shadowColor, radius: settings.shadowRadius)
+             .modifier(
+                 PopoverOffsetVertical(isPresented: isPresented,
+                 popoverType: popoverType,
+                 position: position,
+                xOffset: settings.offset.width,
+                yOffset: settings.offset.height,
+                arrowLength: settings.arrowLength)
+             )
+             .animation(settings.animation)
+          
+      }
      
     internal func arrowPosition(viewPosition: ViewPosition, settings: DYPopoverViewSettings)->ViewPosition {
          
@@ -187,6 +340,73 @@ internal struct PopoverOffset: ViewModifier {
        }
 }
 
+/// An altenative offset modifier which does not depend on CGSize, Useful only for pop over positioned at the top or bottom so that we don't have to deal with left right positioned one.
+internal struct PopoverOffsetVertical: ViewModifier {
+    
+    @Binding var isPresented:Bool
+    var popoverType: PopoverType
+    var position: ViewPosition
+    var xOffset: CGFloat
+    var yOffset: CGFloat
+    var arrowLength: CGFloat
+    
+    func body(content: Content) -> some View {
+        return content
+            .offset(x: xOffset, y: yOffset + arrowLength)
+    }
+    
+    
+    // TODO: - Use the methods below like on the original <PopoverOffset> modifer when there is time.
+    func offsetXFor(position: ViewPosition, frame: CGRect, originBounds: CGRect, arrowLength: CGFloat, addX: CGFloat)->CGFloat {
+           
+             let midX = originBounds.minX + (originBounds.size.width  - frame.size.width) / 2
+         
+           var offsetX: CGFloat = 0
+        
+           switch position {
+               case .top, .bottom:
+                   offsetX = midX
+           case .left:
+               offsetX = originBounds.minX - frame.size.width - arrowLength
+           case .topLeft, .bottomLeft:
+                   offsetX = originBounds.minX - frame.size.width
+           case .right:
+             offsetX = originBounds.minX  + originBounds.size.width + arrowLength
+           case .topRight, .bottomRight:
+                offsetX = originBounds.minX  + originBounds.size.width
+            case .none:
+                offsetX = 0
+           }
+        
+           return offsetX + addX
+       }
+    
+       
+    func offsetYFor(position: ViewPosition, frame: CGRect, originBounds: CGRect, arrowLength: CGFloat, addY: CGFloat)->CGFloat {
+           
+           let midY = originBounds.minY + (originBounds.size.height -  frame.size.height) / 2
+       
+           var offsetY:CGFloat = 0
+               
+               switch position {
+                   case .left, .right:
+                       offsetY =  midY
+                    case .top:
+                        offsetY =  originBounds.minY - frame.size.height - arrowLength
+                    case .topLeft, .topRight:
+                       offsetY =  originBounds.minY - frame.size.height
+                    case .bottom:
+                        offsetY = originBounds.minY  + originBounds.size.height + arrowLength
+                    case .bottomLeft, .bottomRight:
+                       offsetY = originBounds.minY  + originBounds.size.height
+                   case .none:
+                       offsetY = 0
+               }
+           
+           return offsetY + addY
+       }
+}
+
 
 public struct AnchorView: ViewModifier {
     let viewId: String
@@ -221,6 +441,42 @@ public extension View {
     func popoverView<ContentView: View, BackgroundView: View>(content: @escaping ()->ContentView, background: @escaping ()->BackgroundView, isPresented: Binding<Bool>, frame: Binding<CGRect>, anchorFrame: CGRect?,  popoverType: PopoverType, position: ViewPosition, viewId: String, settings:DYPopoverViewSettings = DYPopoverViewSettings())->some View  {
         self.modifier(PopoverViewModifier(contentView: content, backgroundView: background, show: isPresented,  frame: frame, anchorFrame: anchorFrame,  popoverType: popoverType, position: position, viewId: viewId, settings: settings))
     }
+    
+    /**
+     A pop-over view which is meant to be positioned at either top or buttom of the a given view.
+     - Parameter content: the content that shall appear inside the popover view.
+     - Parameter background: Background view of the DYPopover. Don't set a frame or decorations like shadow etc. This will be added through DYPopoverSettings parameter values.
+     - Parameter isPresented: pass in the state binding which determines if the popover should be displayed.
+     - Parameter  frame: the frame of the popover view. As a Binding var, it can be changed during presentation is necessary.
+     - Parameter popoverType: the type of the popover - popout or popover
+     - Parameter position: one of eight different view positions of the popover.
+    - Parameter settings: a DYPopoverViewSettings struct. You can create a settings struct and override each property. If you don't pass in a settings struct, the default values will be used instead.
+     - Returns: the popover view
+    */
+    func popoverViewVertical<ContentView: View, BackgroundView: View>(
+        content: @escaping ()->ContentView,
+        background: @escaping ()->BackgroundView,
+        isPresented: Binding<Bool>,
+        frame: Binding<CGRect>,
+        anchorFrame: CGRect?,
+        popoverType: PopoverType,
+        position: ViewPosition,
+        viewId: String,
+        settings:DYPopoverViewSettings = DYPopoverViewSettings()
+    )->some View  {
+        self.modifier(
+            PopoverViewModifierVertical(contentView: content,
+            backgroundView: background,
+            show: isPresented,
+            frame: frame,
+            anchorFrame: anchorFrame,
+            popoverType: popoverType,
+            position: position,
+            viewId: viewId,
+            settings: settings)
+        )
+    }
+
 }
 
 
